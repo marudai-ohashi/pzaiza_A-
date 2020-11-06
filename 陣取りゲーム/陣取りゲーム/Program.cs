e90e71ff4aa55e5f7cf269b2078a327a5cfd8252
marudai-ohashi <71536911+marudai-ohashi@users.noreply.github.com>
2020-11-06 18:55:21 +0900

    陣取りゲーム201106

--- 陣取りゲーム/陣取りゲーム/Program.cs
+++ 陣取りゲーム/陣取りゲーム/Program.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using System;
+﻿using System;
 using System.Globalization;
 using System.Collections.Generic;
+using System.Dynamic;
 
 namespace 陣取りゲーム
 {
@@ -13,16 +13,18 @@ namespace 陣取りゲーム
             string[] str = Console.ReadLine().Split(' ');
             int height = int.Parse(str[0]);
             int width = int.Parse(str[1]);
-            int num = int.Parse(str[2]);
-            b.Create_masu(height, width);
-            b.SetRL(num);
+
+            int num;
+            string start = Console.ReadLine();
+            if (start == "A") num = 0;
+            else num = 1;
+
+            b.Create_masu(height, width, start);
 
             int flag = 1;
-            num = -1;
-            while (flag>0)
+            while (flag > 0)
             {
                 flag = 0;
-                num++;
                 for(int i = 0; i < height; i++)
                 {
                     for(int j = 0; j < width; j++)
@@ -33,36 +35,69 @@ namespace 陣取りゲーム
                         }
                     }
                 }
+                num++;
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (b.GetNum(i, j) == num)
+                        {
+                            if (b.MarkNum(i, j)) flag++;
+                        }
+                    }
+                }
+                num++;
             }
-
-            b.SetRemark();
-            b.Draw();
+            int anum = b.GetA();
+            int bnum = b.GetB();
+            Console.WriteLine(anum + " " + bnum);
+            if (anum > bnum) Console.WriteLine("A");
+            else Console.WriteLine("B");
         }
     }
 
     class Ban
     {
-        public char[,] ban;
         public int[,] banNum;
         public int height;
         public int width;
-
-        public int[] rlist;
-
-        public char GetC(int y,int x)
-        {
-            return ban[y, x];
-        }
 
         public int GetNum(int y,int x)
         {
             return banNum[y, x];
         }
 
+        public int GetA()
+        {
+            int num = 0;
+            for(int i = 0; i < height; i++)
+            {
+                for(int j = 0; j < width; j++)
+                {
+                    if (banNum[i, j] % 2 == 0) num++;
+                }
+            }
+            return num;
+        }
+
+        public int GetB()
+        {
+            int num = 0;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (banNum[i, j] % 2 == 1) num++;
+                }
+            }
+            return num;
+        }
+
+
         public bool MarkNum(int y,int x)
         {
             int num = banNum[y, x];
-            num++;
+            num += 2;
             int flag = 0;
 
             if (x < width - 1 && banNum[y, x + 1] == -1)
@@ -89,96 +124,26 @@ namespace 陣取りゲーム
             else return false;
         }
 
-        public bool Mark(int y,int x)
-        {
-            int flag = 0;
 
-            if (x < width - 1 && ban[y, x + 1] == '.')
-            {
-                ban[y, x + 1] = '*';
-                flag ++;
-            }
-            if (x > 0 && ban[y, x - 1] == '.')
-            {
-                ban[y, x - 1] = '*';
-                flag++;
-            }
-            if (y > 0 && ban[y - 1, x] == '.')
-            {
-                ban[y - 1, x] = '*';
-                flag++;
-            }
-            if (y < height - 1 && ban[y + 1, x] == '.')
-            {
-                ban[y + 1, x] = '*';
-                flag++;
-            }
-            if (flag > 0) return true;
-            else return false;
-        }
-
-        public void Create_masu(int y, int x)
+        public void Create_masu(int y, int x,string start)
         {
             height = y;
             width = x;
-            ban = new char[height, width];
             banNum = new int[height, width];
             for (int i = 0; i < height; i++)
             {
                 string str = Console.ReadLine();
                 for (int j = 0; j < width; j++)
                 {
-                    ban[i, j] = str[j];
                     if (str[j] == '.') banNum[i, j] = -1;
                     else if (str[j] == '#') banNum[i, j] = -99;
-                    else if (str[j] == '*') banNum[i, j] = 0;
+                    else if (str[j] == 'B') banNum[i, j] = 1;
+                    else if (str[j] == 'A' && start == "A") banNum[i, j] = 0;
+                    else if (str[j] == 'A' && start == "B") banNum[i, j] = 2;
                 }
             }
         }
 
-        public void SetRL(int num)
-        {
-            rlist = new int[num];
-            for(int i = 0; i < num; i++)
-            {
-                rlist[i] = int.Parse(Console.ReadLine());
-            }
-        }
-
-        public void SetRemark()
-        {
-            int num, flag;
-            for(int i = 0; i < height; i++)
-            {
-                for(int j = 0; j < width; j++)
-                {
-                    num = banNum[i, j];
-                    if (num >= 0)
-                    {
-                        flag = 0;
-                        for (int k = 0; k < rlist.Length; k++)
-                        {
-                            if (num == rlist[k]) flag++;
-                        }
-                        if (flag > 0) ban[i, j] = '?';
-                        else ban[i, j] = '*';
-                    }
-                }
-            }
-        }
-
-        public void Draw()
-        {
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    Console.Write(ban[i, j]);
-                }
-                Console.WriteLine();
-            }
-
-        }
 
         public void DrawNum()
         {
