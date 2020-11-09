2551ddb0fc7e699c9ddebe8e8d7c79d01fc5bb1b
marudai-ohashi <71536911+marudai-ohashi@users.noreply.github.com>
2020-11-09 19:25:28 +0900

    STEP: 2 リバーシの操作（縦横）

--- いびつなリバーシ対戦/いびつなリバーシ対戦/Program.cs
+++ いびつなリバーシ対戦/いびつなリバーシ対戦/Program.cs
@@ -28,10 +28,19 @@ namespace いびつなリバーシ対戦
 
         public void Create_masu(int y, int x)
         {
+            string str;
+            
             height = y;
             width = x;
             ban = new char[height, width];
-            for (int i = 0; i < height; i++) for (int j = 0; j < width; j++) ban[i, j] = '.';
+            for (int i = 0; i < height; i++)
+            {
+                str = Console.ReadLine();
+                for (int j = 0; j < width; j++)
+                {
+                    ban[i, j] = str[j];
+                }
+            }
         }
 
         public void Draw_masu()
@@ -48,15 +57,39 @@ namespace いびつなリバーシ対戦
 
         public void Mrak_masu(int y, int x)
         {
-            ban[y, x] = '!';
+            ban[y, x] = '*';
 
-            for (int i = 0; i < height; i++)
+            for (int i = y + 1; i < height; i++)
             {
-                if (ban[i, x] == '.') ban[i, x] = '*';
+                if (ban[i, x] == '*')
+                {
+                    for (int j = y + 1; j <= i; j++) ban[j, x] = '*';
+                    break;
+                }
             }
-            for (int i = 0; i < width; i++)
+            for (int i = y - 1; i >= 0; i--)
             {
-                if (ban[y, i] == '.') ban[y, i] = '*';
+                if (ban[i, x] == '*')
+                {
+                    for (int j = y - 1; j >= i; j--) ban[j, x] = '*';
+                    break;
+                }
+            }
+            for (int i = x + 1; i < width; i++)
+            {
+                if (ban[y, i] == '*')
+                {
+                    for (int j = x + 1; j <= i; j++) ban[y, j] = '*';
+                    break;
+                }
+            }
+            for (int i = x - 1; i >= 0; i--)
+            {
+                if (ban[y, i] == '*')
+                {
+                    for (int j = x - 1; j >= i; j--) ban[y, j] = '*';
+                    break;
+                }
             }
         }
 
