8b31e5f21b37b7469f89395e8178f4aa08931b7a
marudai-ohashi <71536911+marudai-ohashi@users.noreply.github.com>
2020-11-12 14:31:05 +0900

    STEP: 1 累積和の計算

---
+++ 区間の積/区間の積/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 区間の積
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+            string[] str = Console.ReadLine().Split(' ');
+            int sum = 0;
+
+            for(int i = 0; i < num; i++)
+            {
+                Console.WriteLine(sum += int.Parse(str[i]));
+            }
+        }
+    }
+}
